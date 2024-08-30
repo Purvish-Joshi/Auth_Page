6b5903f3c01e5418c03fe971d57f6d212fcfc175
Purvish Joshi <joshipurvish1904@gmail.com>
2024-08-30 17:59:00 +0530

    update the Components

--- src/Components/Sign_in.jsx
+++ src/Components/Sign_in.jsx
@@ -23,54 +23,53 @@ const SignIn = () => {
           </div>
         </div>
         {/* Add your form or content */}
-        <div className=" w-full  flex justify-center items-center md:w-1/2">
-          <div className="text-gray-500 dark:text-white text-xl">
-            <h2 className="font-medium text-center">
-              Sign-in to access your account
-            </h2>
-
-            <form className="flex flex-col p-4">
-              <label
-                htmlFor="email"
-                className="text-[18px] text-black dark:dark:text-white p-2 pl-0"
-              >
-                Username or Email
-              </label>
-              <input
-                type="email"
-                placeholder="Email"
-                className="w-full mb-1 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
+        <div className=" w-full  flex flex-col justify-center items-center md:w-1/2 text-gray-500 dark:text-white text-xl">
+          <form className="flex flex-col p-4 ">
+            <div className="pb-4">
+              <h2 className=" font-medium text-gray-500 dark:text-white text-xl px-3 text-center ">
+                Sign-in to access your account
+              </h2>
+            </div>
+            <label
+              htmlFor="email"
+              className="text-[18px] text-black dark:dark:text-white p-2 pl-0"
+            >
+              Username or Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full mb-1 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
                 dark:border-slate-700"
-              />
+            />
 
-              <label
-                htmlFor="password"
-                className="text-[18px] text-black p-2 dark:text-white pl-0"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full mb-2 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
+            <label
+              htmlFor="password"
+              className="text-[18px] text-black p-2 dark:text-white pl-0"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full mb-2 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
                 dark:border-slate-700"
-              />
+            />
 
-              <button className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 text-[1rem] mt-3">
-                Sign In
-              </button>
+            <button className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 text-[1rem] mt-3">
+              Sign In
+            </button>
 
-              <h4 className="text-gray-500 m-4 text-sm dark:dark:text-white">
-                Don't have an account? {""}
-                <Link
-                  to="/signup"
-                  className="text-black underline dark:text-gray-500"
-                >
-                  Sign up
-                </Link>
-              </h4>
-            </form>
-          </div>
+            <h4 className="text-gray-500 m-4 text-sm dark:dark:text-white">
+              Don't have an account? {""}
+              <Link
+                to="/signup"
+                className="text-black underline dark:text-gray-500"
+              >
+                Sign up
+              </Link>
+            </h4>
+          </form>
         </div>
       </main>
 
--- src/Components/Sign_up.jsx
+++ src/Components/Sign_up.jsx
@@ -22,64 +22,61 @@ const SignUp = () => {
           </div>
         </div>
         {/* Add your form or content */}
-        <div className=" w-full flex justify-center items-center md:w-1/2">
-          <div className="text-gray-500  dark:text-white text-xl ">
-            <h2 className="font-medium text-center">
-              Sign-up to create an account
-            </h2>
-            <form className="flex flex-col p-4">
-              <label
-                htmlFor="username"
-                className="text-[18px] text-black p-2 dark:text-white pl-0"
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                placeholder="Username"
-                className="w-full mb-1 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
+        <div className=" w-full flex flex-col justify-center items-center md:w-1/2 text-gray-500 dark:text-white text-xl">
+          <form className="flex flex-col p-4">
+            <div className="pb-4">
+              <h2 className="font-medium text-gray-500 dark:text-white text-xl px-3 text-center">
+                Sign-up to create an account
+              </h2>
+            </div>
+            <label
+              htmlFor="username"
+              className="text-[18px] text-black p-2 dark:text-white pl-0"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-full mb-1 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
                 dark:border-slate-700"
-              />
-              <label
-                htmlFor="email"
-                className="text-[18px] text-black p-2 dark:text-white pl-0"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                placeholder="Email"
-                className="w-full mb-1 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400 dark:border-slate-700"
-              />
+            />
+            <label
+              htmlFor="email"
+              className="text-[18px] text-black p-2 dark:text-white pl-0"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full mb-1 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400 dark:border-slate-700"
+            />
 
-              <label
-                htmlFor="password"
-                className="text-[18px] text-black p-2 dark:text-white pl-0"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full mb-2 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
+            <label
+              htmlFor="password"
+              className="text-[18px] text-black p-2 dark:text-white pl-0"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full mb-2 p-1 border border-gray-300 rounded pl-2 text-[17px] dark:bg-transparent dark:text-gray-400
                 dark:border-slate-700"
-              />
+            />
 
-              <button className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 text-[1rem] mt-3">
-                Sign In
-              </button>
+            <button className="w-full py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 text-[1rem] mt-3">
+              Sign In
+            </button>
 
-              <h4 className="text-gray-500 m-4 text-sm dark:dark:text-white">
-                Already have an account? {""}
-                <Link
-                  to="/"
-                  className="text-black underline dark:text-gray-500 "
-                >
-                  Sign in
-                </Link>
-              </h4>
-            </form>
-          </div>
+            <h4 className="text-gray-500 m-4 text-sm dark:dark:text-white">
+              Already have an account? {""}
+              <Link to="/" className="text-black underline dark:text-gray-500 ">
+                Sign in
+              </Link>
+            </h4>
+          </form>
         </div>
       </main>
 
